fe18e7126e90daddc72cd0d58fc7db9dbd534f4d
“[ritu <ritu@email.com>
2025-05-23 15:25:28 +0530

    added typescript

---
+++ index.ts
@@ -0,0 +1,56 @@
+// const x: number = 1;
+console.log("kjnj");
+
+// function greet(firstName: string){
+//   console.log("hello"+firstName);
+  
+// }
+
+// greet("ritu");
+
+// function sum(a: number,b: number){
+//   return a+b;
+// }
+
+// const value = sum(1,2)
+// console.log(value);
+
+// function isLegal(age: number){
+//   if(age>18){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+// }
+
+// let x = isLegal(18);
+// console.log(x);
+  
+// interface User{
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   email?: string;
+// }
+ 
+
+// function isLegal(user: User){
+//   if(user.age > 18){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+// }
+
+// isLegal({
+//   firstName: 'Ritu',
+//   lastName: "kumar",
+//   age:20,
+  // email: "fhdghch"
+
+// })
+
+
+ 
